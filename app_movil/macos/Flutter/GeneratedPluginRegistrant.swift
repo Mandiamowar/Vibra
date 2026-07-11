69e0daef1bb7c448695731d293533066fb472108
Mandiamowar <mandiamowar@gmail.com>
2026-07-11 20:51:45 +0200

    feat: Integración NFC real con rootfi_tap_to_pay y pantalla de pago

--- app_movil/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app_movil/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import app_settings
 import flutter_secure_storage_macos
 import local_auth_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
 }
