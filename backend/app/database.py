7e7d04ecc1c46a1bf73d6cb8eea91b8fd95f0fb0
Mandiamowar <mandiamowar@gmail.com>
2026-07-11 20:40:49 +0200

    Corregido database.py para PostgreSQL y cambiado runtime a Python 3.12

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,10 +4,8 @@ from sqlalchemy.orm import sessionmaker
 
 from .config import settings
 
-engine = create_engine(
-    settings.DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(settings.DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
