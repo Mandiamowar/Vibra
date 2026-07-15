f7975ba74a5c1659dda6c36a66e5274a15245902
Mandiamowar <mandiamowar@gmail.com>
2026-07-16 01:21:00 +0200

    feat: facturación B2B completa (backend y app)

--- app_movil/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app_movil/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,9 @@ import Foundation
 import file_selector_macos
 import flutter_secure_storage_macos
 import local_auth_darwin
-import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
-  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
